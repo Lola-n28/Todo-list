e84263d1c1e7737de9196c3261fbac741b84a390
Lola-n28 <lola-n@mail.ru>
2021-01-27 22:45:32 +0600

    added todo closing

--- todo/urls.py
+++ todo/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path("add-book/",add_book,name="add-book"),
     path("delete-todo/<id>/",delete_todo,name="delete-todo"),
     path("mark-todo/<id>/",mark_todo,name="mark-todo"),
+    path("close-todo/<id>/",close_todo,name="close-todo"),
    
 
 
